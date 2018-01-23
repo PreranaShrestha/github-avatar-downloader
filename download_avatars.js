bb2e444283d5e5b0929b89828db8add69398a312
PreranaShrestha <prerana.sh@gmail.com>
2018-01-23 04:31:05 +0000

    implement if no argument is passed in command line

--- download_avatars.js
+++ download_avatars.js
@@ -3,35 +3,37 @@ var secrets = require('./secrets');
 var fs = require('fs');
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
+if(repoOwner === undefined || repoOwner === undefined) {
+  console.log("Arguments not specified");
+} else {
+    function getRepoContributors(repoOwner, repoName, cb) {
+      var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent':'PreranaShrestha',
+          'Authorization': secrets.token
+        }
+      }
+      request(options, function(err, res, body) {
+        cb(err, JSON.parse(body));
 
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent':'PreranaShrestha',
-      'Authorization': secrets.token
+      });
     }
-  }
-  request(options, function(err, res, body) {
-    cb(err, JSON.parse(body));
-
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    if(err) {
+      console.log("Errors:", err);
+    }
+    result.forEach(function(element){
+      console.log(`\n`);
+      downloadImageByURL(element.avatar_url, "avatars/" + element.login + ".jpg");
+    });
   });
-}
-getRepoContributors(repoOwner, repoName, function(err, result) {
-  if(err) {
-    console.log("Errors:", err);
-  }
-  result.forEach(function(element){
-    console.log(`\n`);
-    downloadImageByURL(element.avatar_url, "avatars/" + element.login + ".jpg");
-  });
-});
 
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-    .on('error', function (err) {
-      throw err;
-    })
-    .pipe(fs.createWriteStream(filePath));
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+      .on('error', function (err) {
+        throw err;
+      })
+      .pipe(fs.createWriteStream(filePath));
+  }
 }
