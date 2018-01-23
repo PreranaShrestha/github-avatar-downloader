fd0e55e996c5069ceda4ca9dad70986e43f63b47
PreranaShrestha <prerana.sh@gmail.com>
2018-01-23 02:19:01 +0000

    Add download_avatars function

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,24 @@
 var request = require('request');
-console.log('Welcome to the Github Avatar Downloader!');
-function getRepoContributors(reoOwner, repoName, cb) {
+var secrets = require('./secrets');
 
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent':'PreranaShrestha',
+      'Authorization': secrets.token
+    }
+  }
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+
+  });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
+  result.forEach(function(element){
+    console.log(`\n`);
+    console.log(element.avatar_url);
+  });
   console.log("Errors:", err);
-  console.log("Result:", result);
 });
