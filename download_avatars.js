940e18b75622d2604881fcc41850469876686bc7
PreranaShrestha <prerana.sh@gmail.com>
2018-01-23 00:46:10 +0000

    Add get repo contirbutor function

---
+++ download_avatars.js
@@ -0,0 +1,9 @@
+var request = require('request');
+console.log('Welcome to the Github Avatar Downloader!');
+function getRepoContributors(reoOwner, repoName, cb) {
+
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
